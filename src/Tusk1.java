fd4add6da88605f6b8909ac196cd2f1055ecd565
vent <rfhfcm2009@gmail.com>
2024-10-21 21:19:16 +0300

    init tusk 1

---
+++ src/Tusk1.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Tusk1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+
+        int res = 0;
+        while(number != 0) {
+            res = res * 10 + number % 10;
+            number /= 10;
+        }
+        System.out.println(res);
+
+        //ну я думаю гпт бы посложнее сделал
+    }
+}
